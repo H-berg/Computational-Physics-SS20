01aa968a2511d37f1ff03cd9300d020093e68f7a
katrinbls <katrin.bolsmann@live.de>
2020-05-06 20:52:28 +0200

    Aufgabe 2: Programm

---
+++ Übungsblatt2/Aufgabe2.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cmath>
+
+#include "Eigen/Dense"
+#include "./profiler.h"
+
+using namespace std;
+using namespace Eigen;
+
+//Definition einer Funktion die Schritte 1-3 aus Aufgabenteil a ausführt und die Zeiten als Vektor zurückgibt
+Eigen::VectorXd LUZerlegung(const int N)
+{
+
+  Vector3d t;
+  Profiler::init(3);
+
+  // Schritt 1: Erstellen einer zufälligen N x N Matrix sowie eines N-dimensionalen Vektors
+  VectorXd b = VectorXd::Random(N);
+
+  Profiler::start(0);
+  MatrixXd M = MatrixXd::Random(N,N);
+  Profiler::stop(0);
+  t(0) = Profiler::getTimeInS(0);
+
+  //Schritt 2: LU-Zerlegung
+  Profiler::start(1);
+  PartialPivLU<MatrixXd> LU(M);
+  Profiler::stop(1);
+  t(1) = Profiler::getTimeInS(1);
+
+  //Schritt 3: Lösen des Gleichungssystems
+  Profiler::start(2);
+  VectorXd x = LU.solve(b);
+  Profiler::stop(2);
+  t(2) = Profiler::getTimeInS(2);
+
+  Profiler::resetAll();
+  return t;
+
+};
+
+
+
+int main()
+{
+  const int n = 13; // Verwendung einer logarithmischen Skalierung; also ist NMax = 2^n
+  int N_Max = pow(2, n);
+  MatrixXd A(n+1, 3);
+  VectorXd N(n+1);
+  int index = 0;
+
+  for(int i = 1; i <= N_Max; i = 2*i)
+  {
+    VectorXd t = LUZerlegung(i);
+    A.row(index) = t;
+    index++;
+  }
+
+  ofstream f;
+  f.open("data2b.txt");
+  f << A << "\n";
+  f.flush();
+  f.close();
+
+  return 0;
+}
---
+++ Übungsblatt2/Aufgabe2.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+t1, t2, t3 = np.genfromtxt('data2b.txt', unpack=True)
+N = ([1, 2, 2**2, 2**3, 2**4, 2**5, 2**6, 2**7, 2**8, 2**9, 2**(10), 2**(11), 2**(12), 2**(13)])
+
+plt.loglog(N, t1, label = 'Erstellen der Zufallskomponenten')
+plt.loglog(N, t2, label = 'LU-Zerlegung')
+plt.loglog(N, t3, label = 'Lösen des Gleichungssystems')
+plt.loglog(N, t3+t2+t1, label = 'Gesamtlaufzeit')
+plt.ylabel('Zeit/s')
+plt.xlabel('N')
+plt.legend(loc="best")
+plt.savefig('plot2b.pdf')
+
+#In einem nicht logarithmischen Plot sieht man nochmal dass die LU Zerlegung die meisten Laufzeit beansprucht
+plt.clf()
+x = np.linspace(0, 8000, 5000)
+plt.plot(N, t1, label = 'Erstellen der Zufallskomponenten')
+plt.plot(N, t2, label = 'LU-Zerlegung')
+plt.plot(N, t3, label = 'Lösen des Gleichungssystems')
+plt.plot(N, t3+t2+t1, label = 'Gesamtlaufzeit')
+plt.ylabel('Zeit/s')
+plt.xlabel('N')
+plt.legend(loc="best")
+plt.savefig('plot2c.pdf')
